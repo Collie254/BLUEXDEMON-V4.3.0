5ab8d4bdfbf4db1cfb1cf010deb1d0951f2d76dc
Collie254 <colliericchie@gmail.com>
2025-01-30 23:19:42 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ global.pairingNumber = "254798611895"
 
 
 //DO NOT EDIT OR MODD
-global.botNumber = 254798611895
+global.botNumber = ["254798611895"]
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
